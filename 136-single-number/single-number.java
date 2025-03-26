976705b54a9e40b53aaa11e10b0ad24e02c26ea9
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-03-26 14:55:16 +0530

    Time: 1 ms (99.85%) | Memory: 46.3 MB (35.22%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,15 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int uni = nums[0];
-        // HashSet<Integer> hash = new HashSet<>();
-        // for(int i:nums) {
-        //     nums
-        // }
-        // return uni;
-
         for(int i=1; i<nums.length; i++) {
             int a = nums[i];
-            // int b = nums[i-1];
             uni ^= a;
         }
         return uni;

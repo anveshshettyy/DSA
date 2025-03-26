7647effd510e6ab8a8c671c3482a3adf385f5f8b
Anvesh Shetty <157378881+anveshshettyy@users.noreply.github.com>
2025-03-26 14:25:00 +0530

    Time: 11 ms (71.30%) | Memory: 58.5 MB (32.05%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -31,9 +31,14 @@ class Solution {
 
         HashSet <Integer> hash = new HashSet<>();
 
-        for(int num:nums) {
-            if(hash.contains(num)) return true;
-            hash.add(num);
+        // for(int num:nums) {
+        //     if(hash.contains(num)) return true;
+        //     hash.add(num);
+        // }
+
+        for(int i=0; i<nums.length; i++) {
+            if(hash.contains(nums[i])) return true;
+            hash.add(nums[i]);
         }
         return false;
     } 
